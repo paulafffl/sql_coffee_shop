79cdee9e10415fedac58a96d0c422deee95ffa45
Paula <paulafffl@gmail.com>
2020-04-25 12:14:10 +0100

    Installed packages and commented out the menu router so I can test the employees one first

--- api/api.js
+++ api/api.js
@@ -4,7 +4,7 @@ const apiRouter = express.Router();
 const employeesRouter = require('./employees')
 apiRouter.use('/employees', employeesRouter);
 
-const menusRouter = require('./menus')
-apiRouter.use('/menus', menusRouter);
+//const menusRouter = require('./menus')
+//apiRouter.use('/menus', menusRouter);
 
 module.exports = apiRouter;
