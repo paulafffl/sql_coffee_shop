3d35c23317f5f00bac99610766cc4f83156e26b2
Paula <paulafffl@gmail.com>
2020-04-25 10:19:21 +0100

    Wrote GET all route

--- api/employees.js
+++ api/employees.js
@@ -3,4 +3,16 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+employeesRouter.get('/', (req, res, next) => {
+    db.all(
+        `SELECT * FROM Employee WHERE Employee.is_current_employee = 1`, 
+        (error, rows) => {
+        if(error) {
+            next(error);
+        } else {
+            res.status(200).json({employees : rows});
+        }
+    });
+});
+
 module.exports = employeesRouter;
