72acad0c5a9d5906e7b3900213ce7dcbc3823011
Paula <paulafffl@gmail.com>
2020-04-25 10:12:25 +0100

    Created API Router

---
+++ api/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const apiRouter = express.Router();
+
+const employeesRouter = require('./employees')
+apiRouter.use('/employees', employeesRouter);
+
+const menusRouter = require('./menus')
+apiRouter.use('/menus', menusRouter);
+
+module.exports = apiRouter;
